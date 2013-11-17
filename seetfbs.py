fda7ca7e0c5032f04082ccc687ec21377419279b
Jian-Long Huang <jianlong@ntu.edu.tw>
2013-11-18 02:44:02 +0800

    Enhancement main file

--- seetfbs.py
+++ seetfbs.py
@@ -2,16 +2,19 @@
 
 import os
 import argparse
-from seetfbs import ranking
+from seetfbs.basic import Pattern, PatternSet
+from seetfbs.scoring import PatternScoring
+from seetfbs.ranking import Cluster
+from seetfbs.seqio import parse_fasta_noheader
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-ps', required=True, type=argparse.FileType('r'),
+    parser.add_argument('-pset', required=True,
                         help='Positive set of Chip-Chip sequences in FASTA format')
-    parser.add_argument('-ns', required=True, type=argparse.FileType('r'),
+    parser.add_argument('-nset', required=True,
                         help='Negative set of Chip-Chip sequences in FASTA format')
-    parser.add_argument('-plist', required=True, type=argparse.FileType('r'),
+    parser.add_argument('-plist', required=True,
                         help='Pattern list file')
     parser.add_argument('-seqtype', required=True, choices=['dna', 'rna'],
                         help='The sequence type of the patterns [dna|rna]')
@@ -19,12 +22,40 @@ def main():
                         help='Output directory')
     parser.add_argument('-cpu', type=int, default=1,
                         help='Number of CPUs to perform the analysis (default: 1)')
-    parser.add_argument('-nc', type=int, default=5,
-                        help='Number of clusters in the output (default: 5)')
+    parser.add_argument('-sp', type=int, default=1,
+                        help='The weight of position scoring (default: 1)')
+    parser.add_argument('-cluster', type=int, default=5,
+                        help='Maximum number of clusters in the output (default: 5)')
     args = parser.parse_args()
 
-    if not os.path.exists(args.o):
-        os.mkdir(args.o)
+    if not os.path.exists(args.out):
+        os.makedirs(args.out)
+
+    if args.seqtype == 'dna':
+        reverse_complement = True
+    else:
+        reverse_complement = False
+
+    pattern_set = PatternSet(reverse_complement)
+
+    with open(args.plist, 'r') as fi:
+        for line in fi:
+            pattern = Pattern(line.strip())
+            pattern.build_matchtable_pset(parse_fasta_noheader(args.pset), reverse_complement)
+            pattern.build_matchtable_nset(parse_fasta_noheader(args.nset), reverse_complement)
+            pattern_set.add(pattern)
+
+    pattern_scoring = PatternScoring(args.sp)
+    pattern_scoring.build(pattern_set)
+
+    cluster = Cluster(args.cluster, similarity=0.8)
+    cluster.run(pattern_scoring)
+
+    with open(os.path.join(args.out), 'ranked_patterns.txt', 'w') as fo:
+        for i, j in cluster.results.iteritems():
+            fo.write('{0}: {1}'.format(i, ' '.join([p.sequence for p in j])))
+            fo.flush()
+
 
 if __name__ == '__main__':
     main()
